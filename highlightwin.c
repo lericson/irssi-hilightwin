22586ff950640e8383432c0d5bad176e678bda06
lericson <ludvig@lericson.se>
2009-02-21 00:33:42 +0100

    Start the project off with some header and implementation.

---
+++ highlightwin.c
@@ -0,0 +1,28 @@
+/**
+ * Hilight window enabler.
+ *
+ * Sends all hilights to a certain window, prominently named "hilight".
+ * Idea stolen from cras himself, along with znx. See URL:
+ *   http://scripts.irssi.org/scripts/hilightwin.pl
+ *
+ * (Gee, I hope there are no licensing issues here.)
+ */
+
+#include "hilightwin.h"
+
+static void sig_print_text(TEXT_DEST_REC *dest, const char *text)
+    /* You say it best... when you say nothing at all. */
+}
+
+void hilightwin_init(void) {
+    module_register(MODULE_NAME, "core");
+
+    settings_add_bool(MODULE_NAME, "hilightwin_showprivmsg",    true);
+    settings_check();
+
+    signal_add("print text", (SIGNAL_FUNC)sig_print_text);
+}
+
+void hilightwin_deinit(void) {
+    signal_remove("print text", (SIGNAL_FUNC)sig_print_text);
+}
---
+++ highlightwin.h
@@ -0,0 +1,9 @@
+#include <common.h>
+#include <core/modules.h>
+#include <core/signals.h>
+#include <core/settings.h>
+#include <core/levels.h>
+
+#include <fe-common/core/printtext.h>
+
+#define MODULE_NAME "highlightwin"
