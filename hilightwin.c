146dc24a8ea756369bab9074bf6a4cbdda96bd0e
lericson <ludvig@lericson.se>
2009-02-23 02:26:33 +0100

    Move sum stuff round

--- hilightwin.c
+++ hilightwin.c
@@ -8,17 +8,43 @@
  * (Gee, I hope there are no licensing issues here.)
  */
 
-#include "hilightwin.h"
+#include "module.h"
+#include "module-formats.h"
+
+#include <core/modules.h>
+#include <core/signals.h>
+#include <core/settings.h>
+#include <core/levels.h>
+
+#include <fe-common/core/printtext.h>
+
+#define HILIGHTWIN_NAME "hilight"
 
 static void sig_print_text(TEXT_DEST_REC *dest, const char *text) {
-    /* You say it best... when you say nothing at all. */
-    return;
+    /* TODO Move this to a static global, so as to remove a function call/line
+            printed. */
+    int level = settings_get_level("hilightwin_level");
+
+    if ((dest->level & level) && !(dest->level & MSGLEVEL_NOHILIGHT)) {
+        WINDOW_REC *win = window_find_name(HILIGHTWIN_NAME);
+        TEXT_DEST_REC new_dest;
+
+        if (win == NULL) {
+            printtext(dest->server, NULL, MSGLEVEL_CLIENTNOTICE,
+                      "hilightwin cannot find window");
+            return;
+        }
+
+        /* TODO Some context would be nice, I guess... */
+        printtext_window(win, MSGLEVEL_NOHILIGHT | dest->level,
+                         "%s/%s: %s", dest->server_tag, dest->target, text);
+    }
 }
 
 void hilightwin_init(void) {
     module_register(MODULE_NAME, "core");
 
-    settings_add_bool(MODULE_NAME, "hilightwin_showprivmsg", TRUE);
+    settings_add_level(MODULE_NAME, "hilightwin_level", "HILIGHT");
     settings_check();
 
     signal_add("print text", (SIGNAL_FUNC)sig_print_text);
